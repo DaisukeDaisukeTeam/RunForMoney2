69655d9e4dfeffb53111e85078257e1c774b8c2b
DaisukeDaisuke <akblcmakblcm@gmail.com>
2021-01-03 09:30:48 +0900

    アイテムを正常に追加できないバグを修正

--- src/kametan0730mc/RunForMoney2/item/ItemLoader.php
+++ src/kametan0730mc/RunForMoney2/item/ItemLoader.php
@@ -6,12 +6,15 @@ namespace kametan0730mc\RunForMoney2\item;
 
 use pocketmine\network\mcpe\convert\ItemTranslator;
 use pocketmine\network\mcpe\convert\ItemTypeDictionary;
+use pocketmine\network\mcpe\protocol\types\ItemTypeEntry;
 use pocketmine\utils\Config;
 
 class ItemLoader{
+	public static $item_id_map;
+
 	public static function init(String $confdir){
-		$item_id_map = (new Config($confdir."add_item_id_map.json", Config::JSON))->getAll();
-		self::register($item_id_map);
+		self::$item_id_map = (new Config($confdir."add_item_id_map.json", Config::JSON))->getAll();
+		self::register(self::$item_id_map);
 	}
 
 	public static function register(array $item_id_map){
@@ -21,8 +24,8 @@ class ItemLoader{
 		$simpleCoreToNetMapping = [];
 		$simpleNetToCoreMapping = [];
 
-		$runtimeId = self::bindTo(function(){
-			return max($this->stringToIntMap) + 1;
+		[$runtimeId, $stringToIntMap, $intToStringIdMap, $itemTypes] = self::bindTo(function(){
+			return [max($this->stringToIntMap) + 1, $this->stringToIntMap, $this->intToStringIdMap, $this->itemTypes];
 		}, ItemTypeDictionary::getInstance());
 
 		foreach($item_id_map as $string_id => $id){
@@ -31,21 +34,32 @@ class ItemLoader{
 
 			$simpleCoreToNetMapping[$id] = $runtimeId;
 			$simpleNetToCoreMapping[$runtimeId] = $id;
+	
+			$itemTypes[] = new ItemTypeEntry($string_id, $runtimeId, false);
 			++$runtimeId;
 		}
-
-		self::bindTo(function() use ($stringToIntMap, $intToStringIdMap){
-			$this->stringToIntMap += $stringToIntMap;
-			$this->intToStringIdMap += $intToStringIdMap;
-		},ItemTypeDictionary::getInstance());
 
 		self::bindTo(function() use ($simpleCoreToNetMapping, $simpleNetToCoreMapping){
 			$this->simpleCoreToNetMapping += $simpleCoreToNetMapping;
 			$this->simpleNetToCoreMapping += $simpleNetToCoreMapping;
 		},ItemTranslator::getInstance());
+
+		self::bindTo(function() use ($itemTypes, $stringToIntMap, $intToStringIdMap){
+			$this->stringToIntMap = $stringToIntMap;
+			$this->intToStringIdMap = $intToStringIdMap;
+			$this->itemTypes = $itemTypes;
+		},ItemTypeDictionary::getInstance());
 	}
+
 
 	public static function bindTo(\Closure $closure, $class){
 		return $closure->bindTo($class, get_class($class))();
+	}
+
+	public static function getItem_map(): ?array{
+		if(!isset(self::$item_id_map)){
+			return null;
+		}
+		return self::$item_id_map;
 	}
 }
